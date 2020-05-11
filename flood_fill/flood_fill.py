3a93199e8b8cc87456e3562d250e184801f2d5bd
Farrukh <farruh1996@gmail.com>
2020-05-11 11:01:58 -0700

    Add bfs solution

--- flood_fill/flood_fill.py
+++ flood_fill/flood_fill.py
@@ -1,7 +1,28 @@
 #!/usr/bin/env python3
 
 
-def floodFill(image, sr, sc, newColor):
+from collections import deque
+
+
+def floodFill_bfs(image, sr, sc, newColor):
+
+    color = image[sr][sc]
+    l_r = len(image)
+    l_c = len(image[0])
+
+    if color == newColor:
+        return image
+    q = deque([(sr, sc)])
+    while q:
+        i, j = q.popleft()
+        image[i][j] = newColor
+        for k, l in ((1, 0), (0, 1), (-1, 0), (0, -1)):
+            if 0 <= i + k < l_r and 0 <= j + l < l_c and image[i + k][j + l] == color:
+                q.append((i + k, j + l))
+    return image
+
+
+def floodFill_dfs(image, sr, sc, newColor):
     color = image[sr][sc]
     l_r = len(image)
     l_c = len(image[0])
@@ -20,10 +41,10 @@ def floodFill(image, sr, sc, newColor):
 
 if __name__ == '__main__':
     image = [[0, 0, 0], [0, 1, 1]]
-    image = floodFill(image, 1, 1, 1)
+    image = floodFill_bfs(image, 1, 1, 1)
     assert image == [[0, 0, 0], [0, 1, 1]]
 
     image = [[1, 1, 1], [1, 1, 0], [1, 0, 1]]
-    image = floodFill(image, 1, 1, 2)
+    image = floodFill_bfs(image, 1, 1, 2)
     assert image == [[2, 2, 2], [2, 2, 0], [2, 0, 1]]
     print("All good!!!")
