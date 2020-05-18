95eebfbc09f3fd9a1644f48eec56d3d0bef82594
Farrukh <farruh1996@gmail.com>
2020-05-18 13:21:35 -0700

    Add another solution for string permutation

--- permutation_in_string/str_permutation.py
+++ permutation_in_string/str_permutation.py
@@ -28,3 +28,37 @@ class Solution:
             i += 1
 
         return False
+
+    def checkInclusion_2(self, s1: str, s2: str) -> bool:
+        ''' Optimized count checking'''
+        if len(s1) > len(s2):
+            return False
+
+        pattern = [0] * 26
+        s = [0] * 26
+
+        for i in range(len(s1)):
+            pattern[ord(s1[i]) - ord('a')] += 1
+            s[ord(s2[i]) - ord('a')] += 1
+        matches = 0
+        window = len(s1)
+        for i in range(len(pattern)):
+            if pattern[i] == s[i]:
+                matches += 1
+        for i in range(len(s2) - len(s1)):
+            if matches == 26:
+                return True
+            left = ord(s2[i]) - ord('a')
+            right = ord(s2[i + window]) - ord('a')
+            s[left] -= 1
+            if s[left] == pattern[left]:
+                matches += 1
+            elif s[left] == pattern[left] - 1:
+                matches -= 1
+            s[right] += 1
+            if s[right] == pattern[right]:
+                matches += 1
+            elif s[right] == pattern[right] + 1:
+                matches -= 1
+
+        return matches == 26
