2434c0ad08e78e85a798d91a5ca88b7ebff5b9c8
Farrukh <farruh1996@gmail.com>
2020-05-25 14:17:27 -0700

    Add best time to buy stock

---
+++ best_time_to_buy_stock/best_time.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+from typing import List
+
+
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        if len(prices) < 1:
+            return 0
+
+        max_profit = 0
+        min_price = prices[0]
+
+        for i in range(1, len(prices)):
+            max_profit = max(max_profit, prices[i] - min_price)
+            min_price = min(min_price, prices[i])
+
+        return max_profit
